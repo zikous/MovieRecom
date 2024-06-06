ff6025b2f0e038bdef79ffdae0642df620d195f7
Ait Ben Salah Imad <imad.ait-ben-salah@student-cs.fr>
2024-06-06 09:57:02 +0200

    fixing some bugs on the search menu

--- frontend/src/components/SearchResults/SearchResults.jsx
+++ frontend/src/components/SearchResults/SearchResults.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import './SearchResults.css';
@@ -8,6 +8,8 @@ const SearchResults = () => {
   const searchQuery = new URLSearchParams(location.search).get('q');
   const [searchResults, setSearchResults] = useState([]);
   const loader = useRef(null); // Define the loader ref
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0); // Ensure setTotalPages is defined
 
   const truncateDescription = (description, wordLimit) => {
     const words = description.split(' ');
@@ -17,7 +19,7 @@ const SearchResults = () => {
     return description;
   };
 
-  useEffect(() => {
+  const fetchMovies = (page) => {
     if (searchQuery) {
       const options = {
         method: 'GET',
@@ -26,25 +28,63 @@ const SearchResults = () => {
           query: searchQuery,
           include_adult: 'false',
           language: 'en-US',
-          page: '1',
+          page: page,
         },
         headers: {
           accept: 'application/json',
           Authorization:
             'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo',
-        },
+        }
       };
 
       axios
         .request(options)
         .then(function (response) {
-          setSearchResults(response.data.results);
+          if (page === 1) {
+            setSearchResults(response.data.results);
+          } else {
+            setSearchResults(prevMovies => [...prevMovies, ...response.data.results]);
+          }
+          setTotalPages(response.data.total_pages);
         })
         .catch(function (error) {
           console.error(error);
         });
     }
+  };
+
+  useEffect(() => {
+    if (searchQuery) {
+      setCurrentPage(1);
+      setSearchResults([]);
+      fetchMovies(1);
+    }
   }, [searchQuery]);
+
+  useEffect(() => {
+    if (currentPage > 1) {
+      fetchMovies(currentPage);
+    }
+  }, [currentPage]);
+
+  const handleObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  }, []);
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) observer.observe(loader.current);
+
+    return () => observer.disconnect();
+  }, [handleObserver]);
 
   return (
     <div className="movie__list">
