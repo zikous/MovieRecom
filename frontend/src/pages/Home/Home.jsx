648e8af6a333b05e98736c74c9a147ee05f0a1c3
Bheddar Zakaria <zakaria.bheddar@student-cs.fr>
2024-06-06 17:31:50 +0200

    home v1

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,28 +6,82 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './Home.css';
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const loader = useRef(null);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [popularCurrentPage, setPopularCurrentPage] = useState(1);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [upcomingCurrentPage, setUpcomingCurrentPage] = useState(1);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [topRatedCurrentPage, setTopRatedCurrentPage] = useState(1);
+  const popularLoader = useRef(null);
+  const upcomingLoader = useRef(null);
+  const topRatedLoader = useRef(null);
 
-  const fetchMovies = (page) => {
+  const fetchPopularMovies = (page) => {
     const options = {
       method: 'GET',
       url: 'https://api.themoviedb.org/3/movie/popular',
       params: { language: 'en-US', page: page },
       headers: {
         accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo',
+        Authorization: 'Bearer YOUR_API_KEY',
       },
     };
 
     axios
       .request(options)
       .then(function (response) {
-        setMovies((prevMovies) => [...prevMovies, ...response.data.results]);
-        setTotalPages(response.data.total_pages);
+        setPopularMovies((prevMovies) => [
+          ...prevMovies,
+          ...response.data.results.slice(0, 10), // Limiting to 10 movies
+        ]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const fetchUpcomingMovies = (page) => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/movie/upcoming',
+      params: { language: 'en-US', page: page },
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer YOUR_API_KEY',
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setUpcomingMovies((prevMovies) => [
+          ...prevMovies,
+          ...response.data.results.slice(0, 10), // Limiting to 10 movies
+        ]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const fetchTopRatedMovies = (page) => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/movie/top_rated',
+      params: { language: 'en-US', page: page },
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer YOUR_API_KEY',
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setTopRatedMovies((prevMovies) => [
+          ...prevMovies,
+          ...response.data.results.slice(0, 10), // Limiting to 10 movies
+        ]);
       })
       .catch(function (error) {
         console.error(error);
@@ -35,27 +89,86 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetchMovies(currentPage);
-  }, [currentPage]);
+    fetchPopularMovies(popularCurrentPage);
+    fetchUpcomingMovies(upcomingCurrentPage);
+    fetchTopRatedMovies(topRatedCurrentPage);
+  }, [popularCurrentPage, upcomingCurrentPage, topRatedCurrentPage]);
 
-  const handleObserver = useCallback((entries) => {
+  const handlePopularObserver = useCallback((entries) => {
     const target = entries[0];
     if (target.isIntersecting) {
-      setCurrentPage((prevPage) => prevPage + 1);
+      setPopularCurrentPage((prevPage) => prevPage + 1);
+    }
+  }, []);
+
+  const handleUpcomingObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setUpcomingCurrentPage((prevPage) => prevPage + 1);
+    }
+  }, []);
+
+  const handleTopRatedObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setTopRatedCurrentPage((prevPage) => prevPage + 1);
     }
   }, []);
 
   useEffect(() => {
-    const option = {
+    const popularOption = {
       root: null,
       rootMargin: '20px',
       threshold: 1.0,
     };
-    const observer = new IntersectionObserver(handleObserver, option);
-    if (loader.current) observer.observe(loader.current);
+    const popularObserver = new IntersectionObserver(
+      handlePopularObserver,
+      popularOption
+    );
+    if (popularLoader.current) popularObserver.observe(popularLoader.current);
 
-    return () => observer.disconnect();
-  }, [handleObserver]);
+    return () => popularObserver.disconnect();
+  }, [handlePopularObserver]);
+
+  useEffect(() => {
+    const upcomingOption = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    };
+    const upcomingObserver = new IntersectionObserver(
+      handleUpcomingObserver,
+      upcomingOption
+    );
+    if (upcomingLoader.current)
+      upcomingObserver.observe(upcomingLoader.current);
+
+    return () => upcomingObserver.disconnect();
+  }, [handleUpcomingObserver]);
+
+  useEffect(() => {
+    const topRatedOption = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    };
+    const topRatedObserver = new IntersectionObserver(
+      handleTopRatedObserver,
+      topRatedOption
+    );
+    if (topRatedLoader.current)
+      topRatedObserver.observe(topRatedLoader.current);
+
+    return () => topRatedObserver.disconnect();
+  }, [handleTopRatedObserver]);
+
+  const truncateDescription = (description, wordLimit) => {
+    const words = description.split(' ');
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(' ') + '...';
+    }
+    return description;
+  };
 
   return (
     <>
@@ -67,7 +180,7 @@ const Home = () => {
           infiniteLoop={true}
           showStatus={false}
         >
-          {movies.slice(0, 5).map((movie) => (
+          {popularMovies.slice(0, 10).map((movie) => (
             <Link
               key={movie.id}
               style={{ textDecoration: 'none', color: 'white' }}
@@ -88,13 +201,20 @@ const Home = () => {
                     <i className="fas fa-star" />
                   </span>
                 </div>
-                <div className="posterImage__description">{movie.overview}</div>
+                <div className="posterImage__description">
+                  {truncateDescription(movie.overview, 15)}{' '}
+                  {/* Truncate to 15 words */}
+                </div>
               </div>
             </Link>
           ))}
         </Carousel>
-        <div className="movie-grid">
-          {movies.map((movie) => (
+      </div>
+      <div className="popular-movies">
+        <h2>Popular Movies</h2>
+        <div className="movies-container">
+          {/* Popular movies rendering */}
+          {popularMovies.map((movie) => (
             <div key={movie.id} className="movie-item">
               <Link to={`/movie/${movie.id}`}>
                 <div className="movie-poster">
@@ -104,16 +224,58 @@ const Home = () => {
                   />
                 </div>
                 <div className="movie-description">
-                  <h2>{movie.title}</h2>
-                  <p>Rating: {movie.vote_average}</p>
-                  <p>Release Date: {movie.release_date}</p>
+                  <h3>{movie.title}</h3>
+                  <p>{truncateDescription(movie.overview, 10)}</p>{' '}
+                  {/* Truncate to 10 words */}
                 </div>
               </Link>
             </div>
           ))}
         </div>
-        <div ref={loader} className="loader">
-          <h2>Loading...</h2>
+      </div>
+      <div className="upcoming-movies">
+        <h2>Upcoming Movies</h2>
+        <div className="movies-container">
+          {/* Upcoming movies rendering */}
+          {upcomingMovies.map((movie) => (
+            <div key={movie.id} className="movie-item">
+              <Link to={`/movie/${movie.id}`}>
+                <div className="movie-poster">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                </div>
+                <div className="movie-description">
+                  <h3>{movie.title}</h3>
+                  <p>{truncateDescription(movie.overview, 10)}</p>{' '}
+                  {/* Truncate to 10 words */}
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="top-rated-movies">
+        <h2>Top Rated Movies</h2>
+        <div className="movies-container">
+          {topRatedMovies.map((movie) => (
+            <div key={movie.id} className="movie-item">
+              <Link to={`/movie/${movie.id}`}>
+                <div className="movie-poster">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                </div>
+                <div className="movie-description">
+                  <h3>{movie.title}</h3>
+                  <p>{truncateDescription(movie.overview, 10)}</p>{' '}
+                  {/* Truncate to 10 words */}
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </>
