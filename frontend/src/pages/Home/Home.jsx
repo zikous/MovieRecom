e8c8866e6198792926b82a9acb18c5de3880d3e1
Bheddar Zakaria <zakaria.bheddar@student-cs.fr>
2024-06-06 16:04:33 +0200

    home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,82 +1,123 @@
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './Home.css';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './Home.css';
 
-function Home() {
-  const [movieName, setMovieName] = useState('');
+const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const loader = useRef(null);
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      if (movieName.trim() === '') {
-        setMovies([]);
-
-        return;
-      }
-
-      const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo', // Replace with your actual API key
-        },
-      };
-
-      try {
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/search/movie?query=${movieName}`,
-          options
-        );
-        setMovies(response.data.results);
-      } catch (error) {
-        console.error(error);
-      }
+  const fetchMovies = (page) => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/movie/popular',
+      params: { language: 'en-US', page: page },
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo',
+      },
     };
 
-    const delayDebounceFn = setTimeout(() => {
-      fetchMovies();
-    }, 500);
+    axios
+      .request(options)
+      .then(function (response) {
+        setMovies((prevMovies) => [...prevMovies, ...response.data.results]);
+        setTotalPages(response.data.total_pages);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
-    return () => clearTimeout(delayDebounceFn);
-  }, [movieName]);
+  useEffect(() => {
+    fetchMovies(currentPage);
+  }, [currentPage]);
 
-  const uniqueMovies = Array.from(
-    new Set(movies.map((movie) => movie.title))
-  ).map((title) => {
-    return movies.find((movie) => movie.title === title);
-  });
+  const handleObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  }, []);
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) observer.observe(loader.current);
+
+    return () => observer.disconnect();
+  }, [handleObserver]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Movie Search Engine</p>
-        <input
-          className="movieName"
-          placeholder="Search for a movie"
-          value={movieName}
-          onChange={(event) => setMovieName(event.target.value)}
-        />
-        <p>You Are Currently searching for : {movieName}:</p>
-        <ul>
-          {uniqueMovies.map((movie) => (
-            <li key={movie.id}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.title}
-                style={{ width: '100px', height: '150px' }}
-              />
-              <p>
-                {movie.title} ({movie.release_date})
-              </p>
-            </li>
+    <>
+      <div className="poster">
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+          {movies.slice(0, 5).map((movie) => (
+            <Link
+              key={movie.id}
+              style={{ textDecoration: 'none', color: 'white' }}
+              to={`/movie/${movie.id}`}
+            >
+              <div className="posterImage">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                  alt={movie.original_title}
+                />
+              </div>
+              <div className="posterImage__overlay">
+                <div className="posterImage__title">{movie.original_title}</div>
+                <div className="posterImage__runtime">
+                  {movie.release_date}
+                  <span className="posterImage__rating">
+                    {movie.vote_average}
+                    <i className="fas fa-star" />
+                  </span>
+                </div>
+                <div className="posterImage__description">{movie.overview}</div>
+              </div>
+            </Link>
           ))}
-        </ul>
-      </header>
-    </div>
+        </Carousel>
+        <div className="movie-grid">
+          {movies.map((movie) => (
+            <div key={movie.id} className="movie-item">
+              <Link to={`/movie/${movie.id}`}>
+                <div className="movie-poster">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                </div>
+                <div className="movie-description">
+                  <h2>{movie.title}</h2>
+                  <p>Rating: {movie.vote_average}</p>
+                  <p>Release Date: {movie.release_date}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div ref={loader} className="loader">
+          <h2>Loading...</h2>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
